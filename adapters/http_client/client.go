30692b2e76c1fe720ff1950321d75b2bc9368cb2
Emirhan Ataman <emirhan.ataman@modanisa.com>
2022-02-28 23:29:41 +0300

    add http client to send request to another services

---
+++ adapters/http_client/client.go
@@ -0,0 +1,37 @@
+package http_client
+
+import (
+	"io"
+	"net/http"
+	"time"
+)
+
+type Client struct {
+	Timeout time.Duration
+	client  *http.Client
+}
+
+const (
+	DefaultTimeout = time.Second * 3
+)
+
+func NewClient() *Client {
+	return &Client{
+		client: &http.Client{
+			Timeout: DefaultTimeout,
+		},
+	}
+}
+
+func (c *Client) Get(url string) (*http.Response, error) {
+	return c.client.Get(url)
+}
+
+func (c *Client) Do(method, url string, body io.Reader) (*http.Response, error) {
+	var req, err = http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	return c.client.Do(req)
+}
